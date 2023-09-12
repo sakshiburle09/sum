f6dc0e9257971c242dc4c25ca9d3f4a5b1252ec6
Sakshi Pandurang Burle <133487425+sakshiburle09@users.noreply.github.com>
2023-09-12 07:55:35 +0530

    Add files via upload
    sum without 3rd variable using java

---
+++ sum2num.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+public class sum2num {
+public static void main(String Args[]) {
+	int a, b;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number:");
+	a=sc.nextInt();
+	System.out.println("Enter second number:");
+	b=sc.nextInt();
+	a=a+b;
+	System.out.println("Sum of two number is:"+a);
+}
+}
